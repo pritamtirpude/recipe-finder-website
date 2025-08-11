4b65892d88f467d5b0fb4431c1a4feb4f5101d96
Pritam tirpude <pritam.tirpude9601@gmail.com>
2025-08-11 21:13:27 +0530

    recipe details page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import About from './About/About';
 import Home from './Home/Home';
+import RecipeDetails from './RecipeDetails/RecipeDetails';
 import Recipes from './Recipes/Recipes';
 
-export { About, Home, Recipes };
+export { About, Home, RecipeDetails, Recipes };
