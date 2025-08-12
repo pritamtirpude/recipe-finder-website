c56385ba71b6cd275b310f56a70cbe66a25eefe2
Pritam tirpude <pritam.tirpude9601@gmail.com>
2025-08-12 22:45:52 +0530

    navbar links and some css changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,31 @@
+import { Link, useNavigate } from 'react-router';
+import { navLinks } from '../../util/util';
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
-    <nav className="px-4 py-5 md:px-8 md:py-8 md:pb-5 lg:px-15 lg:py-6.5">
+    <nav className="flex items-center justify-between px-4 py-5 md:px-8 md:py-8 md:pb-5 lg:px-15 lg:py-6.5">
       <div>
         <img src="/assets/images/logo.svg" alt="logo" />
       </div>
+
+      <ul className="flex space-x-10">
+        {navLinks.map((link) => (
+          <li key={link.href}>
+            <Link className="capitalize" to={link.href}>
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <button
+        onClick={() => navigate('/recipes')}
+        className="rounded-10 text-nunito-preset-five cursor-pointer bg-neutral-900 px-4 py-3 font-bold text-white"
+      >
+        Browse recipes
+      </button>
     </nav>
   );
 };
--- src/util/util.ts
+++ src/util/util.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const navLinks = [
+  { href: '/', label: 'home' },
+  { href: '/about', label: 'about' },
+  { href: '/recipes', label: 'recipes' },
+] as const;
