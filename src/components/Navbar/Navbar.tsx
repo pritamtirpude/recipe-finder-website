a93848ebf4a05e07c123e5b6cccfd180d86b019b
Pritam tirpude <pritam.tirpude9601@gmail.com>
2025-08-15 16:21:20 +0530

    navbar changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,47 @@
-import { Link, useNavigate } from 'react-router';
+import { motion } from 'motion/react';
+import { Link, useLocation, useNavigate } from 'react-router';
 import { navLinks } from '../../util/util';
+import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  console.log(pathname);
 
   return (
-    <nav className="flex items-center justify-between px-4 py-5 md:px-8 md:py-8 md:pb-5 lg:px-15 lg:py-6.5">
+    <nav className="flex items-center justify-between border-b border-b-neutral-300 px-4 py-5 md:px-8 md:py-8 md:pb-5 lg:px-15 lg:py-6.5">
       <div>
-        <img src="/assets/images/logo.svg" alt="logo" />
+        <Link to="/">
+          <img src="/assets/images/logo.svg" alt="logo" />
+        </Link>
       </div>
 
-      <ul className="flex space-x-10">
+      <ul className="hidden gap-10 lg:flex lg:items-center">
         {navLinks.map((link) => (
-          <li key={link.href}>
-            <Link className="capitalize" to={link.href}>
+          <motion.li layout className="group relative" key={link.href}>
+            <Link className="text-nunito-preset-six text-neutral-900 capitalize" to={link.href}>
               {link.label}
             </Link>
-          </li>
+            <div className="bg-neutral-orange-500 absolute hidden h-[3px] w-full rounded-full group-hover:block" />
+            {pathname === link.href && (
+              <motion.div
+                layoutId="underline"
+                className="bg-neutral-orange-500 absolute h-[3px] w-full rounded-full"
+              />
+            )}
+          </motion.li>
         ))}
       </ul>
 
       <button
         onClick={() => navigate('/recipes')}
-        className="rounded-10 text-nunito-preset-five cursor-pointer bg-neutral-900 px-4 py-3 font-bold text-white"
+        className="rounded-10 text-nunito-preset-five hidden cursor-pointer bg-neutral-900 px-4 py-3 font-bold text-white focus:outline-offset-4 focus:outline-neutral-900 lg:block"
       >
         Browse recipes
       </button>
+
+      <HamburgerMenu />
     </nav>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import Footer from './Footer/Footer';
+import HamburgerMenu from './HamburgerMenu/HamburgerMenu';
 import Navbar from './Navbar/Navbar';
 
-export { Footer, Navbar };
+export { Footer, HamburgerMenu, Navbar };
